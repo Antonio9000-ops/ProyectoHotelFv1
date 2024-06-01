81179b5ceaa8c226ffe17be8ddfbcb902288f8af
Antonio Salvador <js886175@gmail.com>
2024-06-01 14:57:02 -0500

    Prueba 4

--- src/Modelo/Avance1.java
+++ src/Modelo/Avance1.java
@@ -16,6 +16,7 @@ public class Avance1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Este es un avance");
+        System.out.println("Voy a fusionar ramas despues ");
     }
     
 }
